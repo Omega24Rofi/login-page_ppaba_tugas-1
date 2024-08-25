2f49d172be7376e176d588ab18f37f90eb168cfa
Omega24Rofi <iamrofif@gmail.com>
2024-08-25 23:46:53 +0700

    push login page

---
+++ app/src/main/java/com/example/pertemuan_2/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.pertemuan_2
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pertemuan_2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var number = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            txtCounter.text = number.toString()
+            btnCount.setOnClickListener {
+                number++
+                txtCounter.text = number.toString()
+            }
+
+            btnToast.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Count $number", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+}
